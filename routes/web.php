d40760b588dd8b388d9f131179a06dda1610d708
12312312 <1033628519@qq.com>
2019-01-09 09:38:55 +0800

    购物车

--- routes/web.php
+++ routes/web.php
@@ -18,10 +18,6 @@ Route::get('admin/index','Admin\IndexController@index')->middleware('login');
 Route::get('admin/login','Admin\IndexController@login');
 Route::get('admin/loginout','Admin\IndexController@loginout');
 Route::post('admin/logining','Admin\IndexController@logining');
-// d
-
-
-
 //后台用户管理   叶尚君25-40
 Route::resource('admin/user','Admin\UsersController');
 // 后台订单管理
@@ -39,14 +35,6 @@ Route::resource('/admin/admins','Admin\AdminController')->middleware('admins');
 Route::resource('/admin/power','Admin\PowerController')->middleware('power');
 // 轮播图管理
 Route::resource('/admin/slide','Admin\SlideController')->middleware('slide');
-
-
-
-
-
-
-
-
 //后台商品管理   王润朗  41-50
 Route::resource('/admin/goods','Admin\GoodsController')->middleware('goods');
 //网站配置
@@ -59,11 +47,6 @@ Route::delete('/admin/goods/delete/{id}','Admin\GoodsController@delete')->middle
 Route::resource('/admin/brand','Admin\BrandController')->middleware('brand');
 //后台收货地址管理
 Route::resource('/admin/addr','Admin\AddrController')->middleware('addr');
-
-
-
-
-
 //类别管理   厉常建 51-60
 Route::resource('/admin/cates/index','Admin\CatesController')->middleware('cates');
 Route::get('/admin/cates/index/create/{id}','Admin\CatesController@create')->middleware('cates');
@@ -74,30 +57,13 @@ Route::resource('/admin/comment','Admin\CommentController');
 
 // 前台首页
 Route::get('/','Home\IndexController@index');
-
 // 前台商品列表页
-
 Route::get('home/index/search/{id}','Home\SearchController@sear');
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 //个人中心
 Route::get('home/userinfo','Home\InfoController@userinfo');
 Route::post('/home/uploads','Home\InfoController@uploads');
 Route::post('home/saveinfo/{id}','Home\InfoController@saveinfo');
-
+//商品
 Route::get('home/index/introduction/{id}','Home\SearchController@introduction');
 //商品列表页搜索
 Route::get('home/index/sear','Home\SearchController@search');
@@ -111,18 +77,12 @@ Route::post('/home/registering','Home\LoginController@registering');
 Route::get('/home/registering/setstatus/{id}/{token}','Home\LoginController@setstatus');
 // 手机号注册
 Route::post('/home/insert','Home\LoginController@insert');
-
-
-
-
+//验证码
 Route::get('/home/insert/sendMobileCode','Home\LoginController@sendMobileCode');
-
-
 // 登陆
 Route::get('/home/login','Home\LoginController@login');
 Route::post('/home/implement','Home\LoginController@implement');
 // 退出
 Route::get('/home/logout','Home\LoginController@logout');
-
 // 购物车首页
 Route::get('/home/shopcart','Home\ShopController@shopcart');
