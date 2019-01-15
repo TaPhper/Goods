a89a6f434f36d4ecb3ebd1d45a0e16a5d4fa6aa8
12312312 <1033628519@qq.com>
2019-01-15 14:31:05 +0800

    评论

--- app/Http/Controllers/Home/DealController.php
+++ app/Http/Controllers/Home/DealController.php
@@ -4,11 +4,17 @@ namespace App\Http\Controllers\Home;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-
+use App\Models\Home\Indents;
 class DealController extends Controller
 {
     public function order()
     {
-    	return view('home.userinfo.user.order');
+    	$id = session()->get('login_user')['user_id'];
+    	if($id == NULL){
+    		echo '<script>alert("请先登陆");location.href="/home/login"</script>';
+    	}
+    	$data = Indents::where('user_id',$id)->get();
+    	
+    	return view('home.userinfo.user.order',['data'=>$data]);
     }
 }
---
+++ app/Models/Home/Goods.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Home;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Goods extends Model
+{
+    protected $table = 'goods';
+    protected $primary = 'goods_id';
+}
